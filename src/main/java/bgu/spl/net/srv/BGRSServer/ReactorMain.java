03c27339dc190e9a64f34de9dcd42cb657272589
Shahar Azoulai <azoulais@post.bgu.ac.il>
2021-01-04 21:08:05 +0200

    amen push aharon

--- src/main/java/bgu/spl/net/srv/BGRSServer/ReactorMain.java
+++ src/main/java/bgu/spl/net/srv/BGRSServer/ReactorMain.java
@@ -1,8 +1,50 @@
 package bgu.spl.net.srv.BGRSServer;
 
+import bgu.spl.net.srv.Database;
+import bgu.spl.net.srv.DatabaseObjects.Admin;
+import bgu.spl.net.srv.DatabaseObjects.Student;
+import bgu.spl.net.srv.Server;
+
+import java.io.IOException;
+
 public class ReactorMain {
     public static void main(String[] args) {
 
+
+        try {
+            Database.getInstance().initialize("Courses.txt");
+            Student s1 = new Student("a","1");
+            Student s2 = new Student("b","2");
+            Student s3 = new Student("c","3");
+            Student s4 = new Student("d","4");
+
+            Admin a1 = new Admin("sh","12");
+
+            Database.getInstance().addUser(s1);
+            Database.getInstance().addUser(s2);
+            Database.getInstance().addUser(s3);
+            Database.getInstance().addUser(s4);
+            Database.getInstance().addUser(a1);
+
+
+            s1.courseRegister((short)100);
+            s1.courseRegister((short)101);
+            s1.courseRegister((short)102);
+            s1.courseRegister((short)103);
+            s1.courseRegister((short)110);
+            s2.courseRegister((short)100);
+
+            s2.courseRegister((short)100);
+
+            s2.courseRegister((short)100);
+
+
+
+
+            Server.reactor(3,7777,()->new BGRSMessagingProtocol(),()->new BGRSMessageEncoderDecoder()).serve();
+        }
+
+        catch (IOException e) {}
 
     }
 }
--- src/main/java/bgu/spl/net/srv/BGRSServer/TPCMain.java
+++ src/main/java/bgu/spl/net/srv/BGRSServer/TPCMain.java
@@ -11,41 +11,6 @@ import java.io.IOException;
 public class TPCMain {
     public static void main(String[] args){
 
-        try {
-            Database.getInstance().initialize("Courses.txt");
-            Student s1 = new Student("a","1");
-            Student s2 = new Student("b","2");
-            Student s3 = new Student("c","3");
-            Student s4 = new Student("d","4");
-
-            Admin a1 = new Admin("sh","12");
-
-            Database.getInstance().addUser(s1);
-            Database.getInstance().addUser(s2);
-            Database.getInstance().addUser(s3);
-            Database.getInstance().addUser(s4);
-            Database.getInstance().addUser(a1);
-
-
-            s1.courseRegister((short)100);
-            s1.courseRegister((short)101);
-            s1.courseRegister((short)102);
-            s1.courseRegister((short)103);
-            s1.courseRegister((short)110);
-            s2.courseRegister((short)100);
-
-            s2.courseRegister((short)100);
-
-            s2.courseRegister((short)100);
-
-
-
-
-            Server.reactor(3,7777,()->new BGRSMessagingProtocol(),()->new BGRSMessageEncoderDecoder()).serve();
-        }
-
-        catch (IOException e) {}
-
 
 
     }
